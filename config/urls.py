5a085b7bbb948ce7cb3962812b10fd6644f814a8
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-06 00:04:02 +0200

    Add user authentication to UserProfileView
    The updated code enforces that only authenticated users can access UserProfileView. It does this by adding the django.contrib.auth.decorators' login_required decorator. If users are unauthenticated, they will be redirected to the login page.

--- config/urls.py
+++ config/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import include, path
 
 from apps.user.views import UserProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', UserProfileView.as_view(), name='index'),
+    path('', login_required(UserProfileView.as_view()), name='index'),
     path('users/', include('apps.user.urls', namespace='user')),
     path('accounts/', include('allauth.urls')),
 ]
