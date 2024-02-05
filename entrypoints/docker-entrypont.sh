787a1da9ee7c6e5fdeb7b191c52fed4a82d61f87
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-05 21:46:02 +0200

    Update docker entrypoint for non-interactive migrations and superuser creation
    The `migrate` and `createsuperuser` commands in docker-entrypoint.sh have been updated to run in non-interactive mode by adding the `--noinput` option. This change ensures automation and avoids prompting for user input during docker composition.

--- entrypoints/docker-entrypont.sh
+++ entrypoints/docker-entrypont.sh
@@ -1,11 +1,11 @@
 echo "Migrate..."
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "Collect static files..."
 python manage.py collectstatic --noinput
 
 echo "Create superuser..."
-python manage.py createsuperuser
+python manage.py createsuperuser --noinput
 
 echo "Run server..."
 gunicorn 'config.wsgi' --bind=0.0.0.0:8000 --reload
