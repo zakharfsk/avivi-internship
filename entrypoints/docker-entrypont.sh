d5c77cb8276400df901be3dfbd85959d3fa43e08
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-05 11:55:47 +0200

    Add Docker entrypoint script for deployment

---
+++ entrypoints/docker-entrypont.sh
@@ -0,0 +1,11 @@
+echo "Migrate..."
+python manage.py migrate
+
+echo "Collect static files..."
+python manage.py collectstatic --noinput
+
+echo "Create superuser..."
+python manage.py createsuperuser --noinput
+
+echo "Run server..."
+gunicorn 'config.wsgi' --bind=0.0.0.0:8000
