23cc74912b477cef0ec6e9f6baa70c7716243b6a
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-06 00:05:17 +0200

    Update user and telegram user models
    Updated the user and telegram user models in 'models.py'. Added a '__str__' method to both models, made username, first name, last name, and lang fields optional in TelegramUser. Furthermore, increased the data capacity of the telegram_id field.

--- apps/user/models.py
+++ apps/user/models.py
@@ -7,12 +7,18 @@ class User(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True)
 
+    def __str__(self):
+        return self.username
+
 
 class TelegramUser(models.Model):
-    telegram_id = models.IntegerField(unique=True)
-    username = models.CharField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    lang = models.CharField(max_length=5)
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=255, unique=True, blank=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    lang = models.CharField(max_length=5, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
