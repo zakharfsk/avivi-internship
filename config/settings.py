736efa25449b6eca7f28eef46c4cee7c6b356bb4
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-05 12:05:44 +0200

    Update STATIC_URL and STATIC_ROOT in settings.py

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
