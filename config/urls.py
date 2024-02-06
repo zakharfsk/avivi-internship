461226b9fbf64384bbbaffe8bfe0cd904c8daecb
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-06 21:26:37 +0200

    Remove unused import and class in config/urls.py
    The import of staticfiles_urlpatterns from django.contrib.staticfiles.urls which was not being used in the file was removed. This change leads to cleaner and more efficient code by eliminating unnecessary lines.

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 from apps.user.views import UserProfileView
