1628f504c4d4cade4c0fa85c58f07f619f9660de
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-06 21:26:17 +0200

    Simplify user view imports and classes
    Removed redundant imports and streamlined UserProfileView class in user/views.py. Following these changes, social account authentication process has been streamlined, with a focus on the dispatch method now fetching a SocialAccount object directly and using it to get or create a TelegramUser instance. Removed unnecessary get_context_data method from UserLoginView.

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,14 +1,11 @@
-from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.views import LoginView
-from django.core.handlers.wsgi import WSGIRequest
 from django.urls import reverse_lazy
-from django_dump_die.middleware import dd, dump
 
 from .forms import UserLoginForm, UserRegistrationForm
 from django.views.generic import CreateView, TemplateView
 
-from common.mixins import TelegramUserMixin, TitleMixin
-from .models import TelegramUser, User
+from common.mixins import TitleMixin
+from .models import User
 
 
 class UserLoginView(TitleMixin, LoginView):
@@ -26,21 +23,6 @@ class UserRegistrationCreateView(TitleMixin, CreateView):
     template_name = 'user/register.html'
 
 
-class UserProfileView(TitleMixin, TelegramUserMixin, TemplateView):
+class UserProfileView(TitleMixin, TemplateView):
     title = 'Профіль'
-    tg_user = None
     template_name = 'user/index.html'
-
-    def dispatch(self, request: WSGIRequest, *args, **kwargs):
-        dd(request)
-        if request.session.get('account_authentication_methods')[0].get('provider') == 'telegram':
-            print('telegram')
-            social_user = SocialAccount.objects.get(user=request.user)
-            TelegramUser.objects.get_or_create(
-                telegram_id=social_user.uid,
-                username=social_user.extra_data.get('username', ''),
-                first_name=social_user.extra_data.get('first_name', ''),
-                last_name=social_user.extra_data.get('last_name', ''),
-                lang=social_user.extra_data.get('lang', '')
-            )
-        return super().dispatch(request, *args, **kwargs)
