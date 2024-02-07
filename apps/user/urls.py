45e6c6292bf80f5b9ce54e32894dd9abc030cfdb
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-07 15:25:28 +0200

    Add TelegramUser management views and templates
    Introduced several new classes dedicated to the management of TelegramUser objects. This includes creating, editing, deleting, and listing TelegramUser items. Additionally, access to these views is restricted to authenticated users via the use of login_required decorator on respective URLs.

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from apps.user.views import UserLoginView, UserRegistrationCreateView
+from apps.user import views
 
 app_name = 'user'
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('register/', UserRegistrationCreateView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('register/', views.UserRegistrationCreateView.as_view(), name='register'),
+
+    path('profile/', login_required(views.UserProfileView.as_view()), name='profile'),
+
+    path('tg_users/', login_required(views.ListTelegramUser.as_view()), name='tg_users'),
+    path('tg_users/page/<int:page>/', login_required(views.ListTelegramUser.as_view()), name='tg_users_paginator'),
+    path('tg_users/create/', login_required(views.CreateTelegramUser.as_view()), name='tg_users_create'),
+    path('tg_users/<int:pk>/', login_required(views.DetailTelegramUser.as_view()), name='tg_users_detail'),
+    path('tg_users/<int:pk>/update/', login_required(views.UpdateTelegramUser.as_view()), name='tg_users_update'),
+    path('tg_users/<int:pk>/delete/', login_required(views.DeleteTelegramUser.as_view()), name='tg_users_delete'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
-from .forms import UserLoginForm, UserRegistrationForm
-from django.views.generic import CreateView, TemplateView
+from .forms import TelegramUserForm, UserLoginForm, UserRegistrationForm
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView
 
-from common.mixins import TitleMixin
-from .models import User
+from common.mixins import GroupRequiredMixin, TitleMixin
+from .models import TelegramUser, User
 
 
 class UserLoginView(TitleMixin, LoginView):
     title = 'Авторизація'
     form_class = UserLoginForm
-    success_url = reverse_lazy('index')
-    template_name = 'user/login.html'
+    success_url = reverse_lazy('user:profile')
+    template_name = 'user/auth/login.html'
 
 
 class UserRegistrationCreateView(TitleMixin, CreateView):
@@ -20,9 +20,49 @@ class UserRegistrationCreateView(TitleMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     success_url = reverse_lazy('user:login')
-    template_name = 'user/register.html'
+    template_name = 'user/auth/register.html'
 
 
 class UserProfileView(TitleMixin, TemplateView):
     title = 'Профіль'
     template_name = 'user/index.html'
+
+
+class ListTelegramUser(TitleMixin, ListView):
+    model = TelegramUser
+    paginate_by = 3
+    title = 'Telegram users'
+    template_name = 'user/telegram_users/telegram_users_list.html'
+
+
+class DetailTelegramUser(TitleMixin, GroupRequiredMixin, DetailView):
+    model = TelegramUser
+    title = 'Telegram user detail'
+    template_name = 'user/telegram_users/telegram_user_detail.html'
+    group_required = 'manager'
+
+
+class CreateTelegramUser(TitleMixin, GroupRequiredMixin, CreateView):
+    model = TelegramUser
+    form_class = TelegramUserForm
+    title = 'Create Telegram user'
+    template_name = 'user/telegram_users/telegram_user_create.html'
+    success_url = reverse_lazy('user:tg_users')
+    group_required = 'manager'
+
+
+class UpdateTelegramUser(TitleMixin, GroupRequiredMixin, UpdateView):
+    model = TelegramUser
+    form_class = TelegramUserForm
+    title = 'Update Telegram user'
+    template_name = 'user/telegram_users/telegram_user_update.html'
+    success_url = reverse_lazy('user:tg_users')
+    group_required = 'manager'
+
+
+class DeleteTelegramUser(TitleMixin, GroupRequiredMixin, DeleteView):
+    model = TelegramUser
+    title = 'Delete Telegram user'
+    template_name = 'user/telegram_users/telegram_user_delete.html'
+    success_url = reverse_lazy('user:tg_users')
+    group_required = 'manager'
