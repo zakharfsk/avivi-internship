b9cba1cdfce1364a3ab807c785e6614c33b53f66
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-07 15:28:21 +0200

    Refactor mixins.py to include GroupRequiredMixin
    Implemented GroupRequiredMixin in mixins.py, adding another layer of security to the application. This Mixin checks for user's group membership before allowing access to particular views or pages, increasing the control over who can see what. This effectively helps in managing authorization in a more efficient manner.

--- common/mixins.py
+++ common/mixins.py
@@ -1,7 +1,20 @@
+from django.http import HttpResponseForbidden
+
+
 class TitleMixin:
-    title = None
+    title: str | None = None
 
     def get_context_data(self, **kwargs):
         context = super(TitleMixin, self).get_context_data()
         context['title'] = self.title
         return context
+
+
+class GroupRequiredMixin:
+    group_required: str | None = None
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.groups.filter(name=self.group_required.capitalize()).exists():
+            return super(GroupRequiredMixin, self).dispatch(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden('Forbidden. You are not in the group required.')
