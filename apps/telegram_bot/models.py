7ec9176c05f0f36e50749d29f9e737bda93c0f34
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-09 13:04:14 +0200

    Remove webhook methods from telegram_bot model
    The `set_webhook` and `delete_webhook` methods were removed from the telegram_bot model file. This change was necessary due to restructuring of the application, and these methods and their functionality will now be handled separately in another component of the application. The removal also helps clean up the model file and adhere to the single responsibility principle.

--- apps/telegram_bot/models.py
+++ apps/telegram_bot/models.py
@@ -1,4 +1,3 @@
-import requests
 from django.db import models
 
 
@@ -7,17 +6,6 @@ class TelegramBot(models.Model):
     username = models.CharField(max_length=255)
     token = models.CharField(max_length=255)
     webhook_enabled = models.BooleanField(default=False)
-
-    def set_webhook(self, url: str):
-        response = requests.get(
-            f'https://api.telegram.org/bot{self.token}/setWebhook',
-            params={'url': url}
-        )
-        return response.json()
-
-    def delete_webhook(self):
-        response = requests.get(f'https://api.telegram.org/bot{self.token}/deleteWebhook')
-        return response.json()
 
     def __str__(self):
         return self.name + ' ' + self.username
