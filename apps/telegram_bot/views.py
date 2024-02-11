f314cb0e28cc67122ca77e2603aaeda0a9d0e9f6
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-11 18:26:21 +0200

    Refactor Telegram bot command and language processing
    This update significantly modifies the logic associated with language selection and '/start' command processing in the bot's view. Enhanced functionality now allows for accurate identification of user language preferences and commands with an added feature to respond to unrecognised commands. Callback matching and processing mechanism is also revised for better accuracy and performance.

--- apps/telegram_bot/views.py
+++ apps/telegram_bot/views.py
@@ -6,11 +6,14 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.utils import translation
+from django.utils.translation import get_language_info, gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
-from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ReplyMarkup
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 
-from apps.telegram_bot.callbacks_types import CallBacksTypes
+from apps.telegram_bot.bot_settings.callbacks import CallBacksTypes
+from apps.telegram_bot.bot_settings.commands import BotCommand
 from apps.telegram_bot.forms import TelegramBotForm
 from apps.telegram_bot.models import TelegramBot
 from apps.user.models import TelegramUser
@@ -60,47 +63,63 @@ def telegram_bot_webhook_view(request: WSGIRequest):
 
     load_body = decode_body if decode_body.get("message") else decode_body.get("callback_query")
     is_callback_query = bool(decode_body.get("callback_query"))
+    is_command = load_body['message']['text'].startswith('/')
     type_callback = load_body['data'].split(':')[0] if is_callback_query else None
 
-    if load_body['message']['text'] == '/start':
-        tg_user, created = TelegramUser.objects.get_or_create(
-            telegram_id=load_body['message']['from']['id'],
-            username=load_body['message']['from'].get('username', ''),
-            first_name=load_body['message']['from']['first_name'],
-            last_name=load_body['message']['from'].get('last_name', '')
-        )
+    tg_user = TelegramUser.objects.filter(telegram_id=load_body['message']['from']['id'])
 
-        if created:
-            bot.send_message(
-                text='Choose language',
-                chat_id=load_body['message']['from']['id'],
-                reply_markup=InlineKeyboardMarkup(
-                    inline_keyboard=[
-                        [
-                            InlineKeyboardButton(text='English', callback_data='set_lang:en'),
-                            InlineKeyboardButton(text='Українська', callback_data='set_lang:ua')
-                        ],
-                    ]
+    if tg_user.exists():
+        translation.activate(tg_user.first().lang)
+
+    if is_command:
+        if load_body['message']['text'] == BotCommand.START:
+            if not tg_user.exists():
+                TelegramUser.objects.create(
+                    telegram_id=load_body['message']['from']['id'],
+                    username=load_body['message']['from'].get('username', ''),
+                    first_name=load_body['message']['from']['first_name'],
+                    last_name=load_body['message']['from'].get('last_name', '')
                 )
-            )
+                bot.send_message(
+                    text='Choose language',
+                    chat_id=load_body['message']['from']['id'],
+                    reply_markup=InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [
+                                InlineKeyboardButton(text='English', callback_data='set_lang:en'),
+                                InlineKeyboardButton(text='Українська', callback_data='set_lang:uk')
+                            ],
+                        ]
+                    )
+                )
+            else:
+                bot.send_message(
+                    chat_id=load_body['message']['from']['id'],
+                    text=str(_('tg_bot_greeting'))
+                )
         else:
             bot.send_message(
                 chat_id=load_body['message']['from']['id'],
-                text='Welcome' if tg_user.lang == 'en' else 'Вітаю'
+                text=str(_('tg_bot_unknown_command'))
             )
 
     if is_callback_query:
         if type_callback == CallBacksTypes.SET_LANG:
-            TelegramUser.objects.filter(telegram_id=load_body['from']['id']).update(lang=load_body['data'])
+            TelegramUser.objects.filter(telegram_id=load_body['from']['id']).update(
+                lang=load_body['data'].split(':')[1]
+            )
+            translation.activate(load_body['data'].split(':')[1])
+
             bot.edit_message_text(
                 chat_id=load_body['from']['id'],
                 message_id=load_body['message']['message_id'],
-                text='Вітаю' if load_body['data'] == 'ua' else 'Welcome'
+                text=str(_('tg_bot_greeting'))
             )
 
         if type_callback == CallBacksTypes.CHANGE_LANG:
             pass
 
+    translation.deactivate()
     return JsonResponse({'status': 'ok'})
 
 
