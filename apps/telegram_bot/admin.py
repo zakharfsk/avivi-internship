309eee1763625038f80f3219b7b117bbb3bfacf9
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-11 18:26:00 +0200

    Update fields in telegram_bot admin
    Added 'webhook_enabled' to the fields list in telegram_bot's admin.py. This enhancement allows admin users to more easily manage the webhook status of bots.

--- apps/telegram_bot/admin.py
+++ apps/telegram_bot/admin.py
@@ -8,4 +8,4 @@ class TelegramBotAdmin(admin.ModelAdmin):
     list_display = ('name', 'username', 'token', 'webhook_enabled')
     search_fields = ('name', 'username')
     list_filter = ('name', 'username')
-    fields = ('name', 'username', 'token')
+    fields = ('name', 'username', 'token', 'webhook_enabled')
