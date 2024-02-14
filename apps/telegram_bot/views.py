c5abfcb836c89f3ff0b62e936548758dfa850b05
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-14 14:25:48 +0200

    Refactor telegram_bot_webhook_view to use UpdateHandler
    Introduced the UpdateHandler class in telegram_bot_webhook_view function to reduce complexity and improve code readability. This led to a significant reduction in lines of code as the processing of the incoming JSON, identification of the type of the incoming request and its validation, is now being handled in the UpdateHandler class.

--- apps/telegram_bot/views.py
+++ apps/telegram_bot/views.py
@@ -14,6 +14,7 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 
 from apps.telegram_bot.bot.callbacks import TypeCallBacks
 from apps.telegram_bot.bot.commands import BotCommand
+from apps.telegram_bot.bot.handler import UpdateHandler
 from apps.telegram_bot.forms import TelegramBotForm
 from apps.telegram_bot.models import TelegramBot
 from apps.user.models import TelegramUser
@@ -58,66 +59,14 @@ class TelegramBotDeleteView(TitleMixin, LoginRequiredMixin, SuperUserRequiredMix
 
 @csrf_exempt
 def telegram_bot_webhook_view(request: WSGIRequest):
-    decode_body = json.loads(request.body.decode('utf-8'))
     bot = Bot(token=TelegramBot.objects.first().token)
+    decode_body = json.loads(request.body.decode('utf-8'))
 
-    load_body = decode_body if decode_body.get("message") else decode_body.get("callback_query")
-    is_callback_query = bool(decode_body.get("callback_query"))
-    is_command = load_body['message']['text'].startswith('/')
-    type_callback = load_body['data'].split(':')[0] if is_callback_query else None
-
-    tg_user = TelegramUser.objects.filter(telegram_id=load_body['message']['from']['id'])
-
-    if tg_user.exists():
-        translation.activate(tg_user.first().lang)
-
-    if is_command:
-        if load_body['message']['text'] == BotCommand.START:
-            if not tg_user.exists():
-                TelegramUser.objects.create(
-                    telegram_id=load_body['message']['from']['id'],
-                    username=load_body['message']['from'].get('username', ''),
-                    first_name=load_body['message']['from']['first_name'],
-                    last_name=load_body['message']['from'].get('last_name', '')
-                )
-                bot.send_message(
-                    text='Choose language',
-                    chat_id=load_body['message']['from']['id'],
-                    reply_markup=InlineKeyboardMarkup(
-                        inline_keyboard=[
-                            [
-                                InlineKeyboardButton(text='English', callback_data='set_lang:en'),
-                                InlineKeyboardButton(text='Українська', callback_data='set_lang:uk')
-                            ],
-                        ]
-                    )
-                )
-            else:
-                bot.send_message(
-                    chat_id=load_body['message']['from']['id'],
-                    text=str(_('tg_bot_greeting'))
-                )
-        else:
-            bot.send_message(
-                chat_id=load_body['message']['from']['id'],
-                text=str(_('tg_bot_unknown_command'))
-            )
-
-    if is_callback_query:
-        if type_callback == TypeCallBacks.SET_LANG:
-            TelegramUser.objects.filter(telegram_id=load_body['from']['id']).update(
-                lang=load_body['data'].split(':')[1]
-            )
-            translation.activate(load_body['data'].split(':')[1])
-
-            bot.edit_message_text(
-                chat_id=load_body['from']['id'],
-                message_id=load_body['message']['message_id'],
-                text=str(_('tg_bot_greeting'))
-            )
-
-        if type_callback == TypeCallBacks.CHANGE_LANG:
-            pass
+    updater = UpdateHandler(
+        decode_body,
+        bot
+    )
+    updater.handle()
 
     translation.deactivate()
     return JsonResponse({'status': 'ok'})
