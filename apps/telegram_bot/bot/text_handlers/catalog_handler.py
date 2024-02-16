77a532a733dd0759bf4e74ef6b93bc3cee12fb9c
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-16 14:56:11 +0200

    Update and implement pagination for Telegram bot catalog
    Updated the pagination functionality for the catalog of the Telegram bot. The changes include enhancing the typing of the updater object, refining how the pages are fetched, adding the ability to switch pages, and modifying the existing keyboard options. This update improves an overall navigation through the catalog.

--- apps/telegram_bot/bot/text_handlers/catalog_handler.py
+++ apps/telegram_bot/bot/text_handlers/catalog_handler.py
@@ -1,28 +1,43 @@
+from typing import TYPE_CHECKING
+
 from django.core.paginator import Paginator
 from django.utils.translation import gettext_lazy as _
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from apps.product.models import Category
 
+if TYPE_CHECKING:
+    from apps.telegram_bot.bot.handler import UpdaterHandler
+
 
 class CatalogHandler:
     def __init__(self, updater):
-        self.updater = updater
+        self.updater: 'UpdaterHandler' = updater
 
-    def handle(self):
+    def handle(self, page=1):
+        self.get_category_pagination().get_page(page)
         self.updater.bot.send_message(
             chat_id=self.updater.body['from']['id'],
             text=str(_('tg_bot_category_list')),
-            reply_markup=self.categories_keyboard()
+            reply_markup=self.categories_keyboard(page)
+        )
+
+    def change_page(self, page):
+        self.updater.bot.edit_message_reply_markup(
+            chat_id=self.updater.body['message']['chat']['id'],
+            message_id=self.updater.body['message']['message_id'],
+            reply_markup=self.categories_keyboard(page)
         )
 
     @staticmethod
-    def get_pagination():
-        return Paginator(Category.objects.all(), 3)
+    def get_category_pagination(per_page=3):
+        return Paginator(Category.objects.all(), per_page)
 
-    def categories_keyboard(self, page=1):
-        paginator = self.get_pagination().get_page(page)
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[])
+    def categories_keyboard(self, page):
+        paginator = self.get_category_pagination().get_page(page)
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text=str(_('tg_bot_product_list')), callback_data='show_products')]
+        ])
 
         keyboard.inline_keyboard.extend(
             [
@@ -35,7 +50,7 @@ class CatalogHandler:
             keyboard.inline_keyboard.append([
                 InlineKeyboardButton(
                     text='Next',
-                    callback_data=f'list_categories_next_page:{paginator.next_page_number()}'
+                    callback_data=f'list_categories_change_page:{paginator.next_page_number()}'
                 )
             ])
 
@@ -43,7 +58,7 @@ class CatalogHandler:
             keyboard.inline_keyboard.append([
                 InlineKeyboardButton(
                     text='Previous',
-                    callback_data=f'list_categories_prev_page:{paginator.previous_page_number()}'
+                    callback_data=f'list_categories_change_page:{paginator.previous_page_number()}'
                 )
             ])
 
