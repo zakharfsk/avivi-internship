c549834f8917cf3d16577cf33bb8adaf3c65ccfb
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-19 21:11:29 +0200

    Add two-factor authentication handler to Telegram bot
    This update ensures enhanced security by implementing a handler for two-factor authentication to the bot's state. The new `GetTwoAuthCodeHandler` handles the additional authentication stage, which is part of the newly added states in `states.py`.

--- apps/telegram_bot/bot/handler.py
+++ apps/telegram_bot/bot/handler.py
@@ -8,6 +8,8 @@ from apps.telegram_bot.bot.callbacks_handlers.setlang_callback import SetLangCal
 from apps.telegram_bot.bot.callbacks_handlers.show_products import ProductsHandler
 from apps.telegram_bot.bot.commands import BotCommand
 from apps.telegram_bot.bot.commands_handlers.start import StartHandler
+from apps.telegram_bot.bot.states import State
+from apps.telegram_bot.bot.states_handler.enter_two_code_state import GetTwoAuthCodeHandler
 from apps.telegram_bot.bot.text_commands import KeyboardTextCommand
 from apps.telegram_bot.bot.text_handlers.catalog_handler import CatalogHandler
 from apps.user.models import TelegramUser
@@ -26,6 +28,11 @@ class UpdaterHandler:
 
         if BotCommand.START == self.get_command():
             StartHandler(self).handle()
+            return
+
+        if self.get_tg_user().state == State.ENTER_TWO_AUTH_CODE:
+            GetTwoAuthCodeHandler(self).handle()
+            return
 
         if self.get_callback_type() == TypeCallBacks.SET_LANG:
             SetLangCallBack(self).handle()
@@ -48,6 +55,11 @@ class UpdaterHandler:
         )
         translation.activate(tg_user.first().lang if tg_user.exists() else 'en')
 
+    def get_tg_user(self):
+        return TelegramUser.objects.get(
+            telegram_id=self.body['from']['id']
+        )
+
     @staticmethod
     def is_command(message: dict):
         return True if message.get('message', {}).get('entities') else False
@@ -57,7 +69,7 @@ class UpdaterHandler:
         return True if 'message' in message else False
 
     @staticmethod
-    def is_callback(message):
+    def is_callback(message: dict):
         return True if 'callback_query' in message else False
 
     @staticmethod
