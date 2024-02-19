8686e4ff1d6e6893fdac098c3171b2df3dde67e5
zakharfsk <fesikzakhar@khmnu.edu.ua>
2024-02-19 21:11:43 +0200

    Revise language setting and add 2FA in Telegram bot
    Updated the language setting mechanism in Telegram bot and introduced two-factor authentication (2FA) for increased security. The updated code now prompts for a 2FA code when the bot's 2FA setting is not enabled, enhancing the safety features of the bot.

--- apps/telegram_bot/bot/callbacks_handlers/setlang_callback.py
+++ apps/telegram_bot/bot/callbacks_handlers/setlang_callback.py
@@ -1,10 +1,13 @@
 from typing import TYPE_CHECKING
 
+from django.conf import settings
 from django.utils import translation
 from django.utils.translation import gettext_lazy as _
+from telegram import ParseMode
 
 from apps.telegram_bot.bot.commands_handlers.start import StartHandler
-from apps.user.models import TelegramUser
+from apps.telegram_bot.bot.states import State
+from apps.telegram_bot.bot.utils import get_qr_image
 
 if TYPE_CHECKING:
     from apps.telegram_bot.bot.handler import UpdaterHandler
@@ -16,14 +19,28 @@ class SetLangCallBack:
 
     def handle(self):
         lang = self.updater.body['data'].split(':')[1]
-        TelegramUser.objects.filter(telegram_id=self.updater.body['from']['id']).update(
-            lang=lang
-        )
+        tg_user = self.updater.get_tg_user()
+        tg_user.lang = lang
 
         translation.activate(lang)
-        self.updater.bot.edit_message_text(
+
+        self.updater.bot.delete_message(
             chat_id=self.updater.body['from']['id'],
-            message_id=self.updater.body['message']['message_id'],
+            message_id=self.updater.body['message']['message_id']
+        )
+        self.updater.bot.send_message(
+            chat_id=self.updater.body['from']['id'],
             text=str(_('tg_bot_greeting')),
             reply_markup=StartHandler.start_reply_keyboard()
         )
+
+        if not tg_user.two_auth_enabled:
+            self.updater.bot.send_photo(
+                chat_id=self.updater.body['from']['id'],
+                caption=str(_('tg_bot_two_auth_not_enabled')).format(code=settings.OTP_SECRET_KEY),
+                photo=get_qr_image(),
+                parse_mode=ParseMode.HTML
+            )
+            tg_user.state = State.ENTER_TWO_AUTH_CODE
+
+        tg_user.save()
