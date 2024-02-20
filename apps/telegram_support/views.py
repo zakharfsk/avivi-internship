8ae0736784078927772cf4d2d8a64ffa2d048096
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-20 12:31:23 +0200

    Add support tickets view and templates
    Implemented a support tickets view and templates for managers. The view lists all support tickets created by users, with pagination, and it is only accessible for managers. The corresponding link has also been added to the base template for navigational purposes.

--- apps/telegram_support/views.py
+++ apps/telegram_support/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from apps.telegram_support.models import Ticket
+from common.mixins import GroupRequiredMixin, TitleMixin
+
+
+class SupportTicketsListView(TitleMixin, LoginRequiredMixin, GroupRequiredMixin, ListView):
+    title = 'Support tickets'
+    model = Ticket
+    template_name = 'support/tickets_list.html'
+    context_object_name = 'tickets'
+    group_required = 'manager'
+    paginate_by = 10
+    ordering = ['-created_at']
