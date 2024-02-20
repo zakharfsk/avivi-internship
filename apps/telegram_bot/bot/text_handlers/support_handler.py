269749ed24506b71a834f23532ea9742b0c9ac62
Zakhar <fesikzakhar@khmnu.edu.ua>
2024-02-20 12:33:24 +0200

    Update support handler in Telegram bot for internationalization
    The code update in the support handler of the Telegram bot allows for multi-language support. The text messages are replaced with gettext_lazy function calls, enabling the bot to send responses in different languages based on user settings. The code also saves ticket details temporarily on each state transition, allowing the bot to build the ticket progressively.

--- apps/telegram_bot/bot/text_handlers/support_handler.py
+++ apps/telegram_bot/bot/text_handlers/support_handler.py
@@ -1,6 +1,8 @@
 from typing import TYPE_CHECKING
 
 from apps.telegram_bot.bot.states import State
+from django.utils.translation import gettext_lazy as _
+from apps.telegram_bot.bot.two_auth import verify_totp
 from apps.telegram_support.models import Ticket
 
 if TYPE_CHECKING:
@@ -13,27 +15,52 @@ class SupportHandler:
 
     def handle(self):
         tg_user = self.updater.get_tg_user()
-        ticket, _ = Ticket.objects.get_or_create(user=tg_user)
 
         if tg_user.state == State.NONE_STATE:
             self.updater.bot.send_message(
                 chat_id=tg_user.telegram_id,
-                text='Write your title message'
+                text=str(_('tg_bot_support_title'))
             )
             tg_user.state = State.WRITE_SUPPORT_TITLE
+            tg_user.state_data = {}
+            tg_user.save()
+            return
 
         if tg_user.state == State.WRITE_SUPPORT_TITLE:
             self.updater.bot.send_message(
                 chat_id=tg_user.telegram_id,
-                text='Write your description message'
+                text=str(_('tg_bot_support_description'))
             )
-            ticket.title = self.updater.body['text']
+            tg_user.state_data['title'] = self.updater.body['text']
             tg_user.state = State.WRITE_SUPPORT_DESCRIPTION
+            tg_user.save()
+            return
 
         if tg_user.state == State.WRITE_SUPPORT_DESCRIPTION:
-            ticket.description = self.updater.body['text']
+            tg_user.state_data['description'] = self.updater.body['text']
+
             self.updater.bot.send_message(
                 chat_id=tg_user.telegram_id,
-                text='Your ticket was created'
+                text=str(_('tg_bot_support_two_auth'))
+            )
+
+            tg_user.state = State.WRITE_SUPPORT_ENTER_TWO_AUTH_CODE
+            tg_user.save()
+            return
+
+        if tg_user.state == State.WRITE_SUPPORT_ENTER_TWO_AUTH_CODE:
+            if not verify_totp(self.updater.body['text']):
+                self.updater.bot.send_message(
+                    chat_id=self.updater.body['from']['id'],
+                    text=str(_('tg_bot_wrong_code'))
+                )
+                return
+
+            Ticket.objects.create(**tg_user.state_data, user=tg_user)
+            self.updater.bot.send_message(
+                chat_id=self.updater.body['from']['id'],
+                text=str(_('tg_bot_ticket_created'))
             )
             tg_user.state = State.NONE_STATE
+            tg_user.state_data = {}
+            tg_user.save()
